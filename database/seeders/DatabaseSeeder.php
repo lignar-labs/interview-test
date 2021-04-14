52131c5df126a73496048f196b9d984d1b8689ab
Shariffuddin_Hussin <mirage3695@gmail.com>
2021-04-14 10:57:09 +0800

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //TODO: seed 10 user
+        for ($i=0; $i < 10; $i++) { 
+        DB::table('users')->insert([
+            'name' => Str::random(8),
+            'email' => Str::random(8).'@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        }
     }
 }
